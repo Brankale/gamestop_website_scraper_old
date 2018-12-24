3b689c4260d82560f6bb8d51a053847098103542
Brankale <45881310+Brankale@users.noreply.github.com>
2018-12-24 20:22:54 +0100

    Add files via upload

---
+++ src/gamestopapp/GamePreview.java
@@ -0,0 +1,48 @@
+package gamestopapp;
+
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+public class GamePreview {
+    
+    private String title;
+    private String url;
+    private String platform;    // not definitive
+    private Image image;        // not definitive
+
+    public GamePreview(String title, String url, String platform, String image) throws MalformedURLException, IOException {
+        this.title = title;
+        this.url = url;
+        this.platform = platform;
+        this.image = ImageIO.read( new URL(image) );
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public String toString() {
+        return  "Title: " + title + "\n" +
+                "URL: " + url + "\n" +
+                "Platform: " + platform + "\n";
+    }
+    
+    
+    
+}
