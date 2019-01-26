eb5f5a15ec7ad475a268010565a0f4898a88be57
Brankale <45881310+Brankale@users.noreply.github.com>
2019-01-26 22:12:45 +0100

    Comparator classes for Game class + sort() + add()
    Four new Comparator classes has been added:
    - GameNewPriceComparator to sort games by new price
    - GameUsedPriceComparator to sort games by used price
    - GamePlatformComparator to sort games by platform
    - GameReleaseDateComparator to sort games by release date
    
    In class Games has been added sort methods:
    - sortbyName()
    - sortByNewPrice()
    - sortByUsedPrice()
    - sortByPlatform()
    - sortByReleaseDate()
    
    in Games class:
    - the add() method has been modified to prevent to insert the same game two times

--- src/gamestopapp/GamestopApp.java
+++ src/gamestopapp/GamestopApp.java
@@ -11,7 +11,8 @@ public class GamestopApp {
             
             Games wishlist = new Games();
             
-            wishlist.add( new Game("https://www.gamestop.it/PS4/Games/110143/detroit-become-human") );            
+            wishlist.add( new Game("https://www.gamestop.it/PS4/Games/110143/detroit-become-human") );
+            wishlist.add( new Game("https://www.gamestop.it/PS4/Games/110143/detroit-become-human") );    
             wishlist.add( new Game("https://www.gamestop.it/PS4/Games/101550/persona-5-steelbook-launch-edition") );
             wishlist.add( new Game("https://www.gamestop.it/XboxONE/Games/112463/forza-horizon-4-deluxe-edition") );
             wishlist.add( new Game("https://www.gamestop.it/PS3/Games/31910/persona-4-arena-limited-edition") );
@@ -19,10 +20,14 @@ public class GamestopApp {
             wishlist.add( new Game("https://www.gamestop.it/PS4/Games/34052/gta-v") );
             
             
-            System.out.println( wishlist.toString() );            
-            wishlist.exportToBinary();
             
-            Games temp = Games.importFromBinary();
+            
+            
+            
+            //System.out.println( wishlist.toString() );            
+            //wishlist.exportToBinary();
+            
+            //Games temp = Games.importFromBinary();
             //System.out.println( temp.toString() );
             
 
